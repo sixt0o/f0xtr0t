d679e2ba2aa93f56126400c3736fde43f9d23315
sixt0o <tjbishop+foxtrot@gmail.com>
2022-01-25 21:05:44 -0700

    update alert and notification placement

--- foxtrot.py
+++ foxtrot.py
@@ -47,7 +47,7 @@ class Foxtrot(plugins.Plugin):
     ALREADY_SENT = list()
     SKIP = list()
     UPDATE_URL = "https://raw.githubusercontent.com/sixt0o/f0xtr0t/main/version.txt"
-    CURRENT_VERSION = "v1.2.8-alpha"
+    CURRENT_VERSION = "v1.2.9-alpha"
 
     def __init__(self):
         self.ready = False
